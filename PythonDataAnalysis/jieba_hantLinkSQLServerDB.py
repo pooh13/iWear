e3818ab4534fce38af9ec46fcbc2c978b6b6fd13
Eason Fang <10456009@ntub.edu.tw>
2019-05-15 21:22:11 +0800

    Update Python Data Analysis

--- PythonDataAnalysis/jieba_hantLinkSQLServerDB.py
+++ PythonDataAnalysis/jieba_hantLinkSQLServerDB.py
@@ -1,72 +1,51 @@
-# Connect SQL
 import pyodbc
 import MyJieba_hant
 
-def cursor2List(s):
-    cursor.execute(s)
+def cursorToList(SQLCmd):
+    cursor.execute(SQLCmd)
     result = cursor.fetchall()
-    listC=[]
+    listResult=[]
     for row in result:
-        listA = [row]
-        string = ",".join(str(v) for v in listA)
+        listRow = [row]
+        string = ",".join(str(v) for v in listRow)
         string=string.replace(" ","").replace("'","").replace("(","").replace(")","")
         listCut = string.split(',')
-        listC.append(listCut)
-    return listC
+        listResult.append(listCut)
+    return listResult
 
-iWear = pyodbc.connect(
+iWearDB = pyodbc.connect(
     'DRIVER={SQL Server}; SERVER=140.131.114.241,1433; DATABASE=108-510; UID=108iwear; PWD=@108iwear',
     ENGINE = "sql_server.pyodbc",
     NAME= "108-510",
-    HOST="140.131.114.241",
+    HOST="140.131.114.241,1433",
     USER="108iwear",
     PASSWORD="@108iwear",
 )
 
-cursor = iWear.cursor()
+cursor = iWearDB.cursor()
 
-# Read
-# JiebaCutAnalyze
-
-# for row in result:
-#     listA = [row]
-#     string = ",".join(str(v) for v in listA)
-#     string=string.replace(" ","").replace("'","").replace("(","").replace(")","")
-#
-#     listCut = string.split(',')
-#     print(listCut)
-#     styleNo = listCut[7]
-#     MyJieba_hant.MyJieba_hant(context=listCut[5])
-
-for row in cursor2List("SELECT memInform.account, memInform.gender, memInform.birth, memInform.height, memInform.weight, post.word, post.photo, post.styleNo, post.accessoriesNo, post.clothesNo, post.coatNo, post.pantsNo, post.shoesNo FROM dbo.memInform as memInform left join dbo.post as post on memInform.account=post.account"):
+for row in cursorToList("SELECT memInform.account, memInform.gender, memInform.birth, memInform.height, memInform.weight, post.word, post.photo, post.styleNo, post.accessoriesNo, post.clothesNo, post.coatNo, post.pantsNo, post.shoesNo FROM dbo.memInform as memInform left join dbo.post as post on memInform.account=post.account"):
     MyJieba_hant.MyJieba_hant(context=row[5])
 
-listB=['styleNo','accessoriesNo','clothesNo','coatNo','pantsNo','shoesNo']
-list_percent=[30,10,15,15,15,15]
+listPostStyleOption=['styleNo','accessoriesNo','clothesNo','coatNo','pantsNo','shoesNo']
+listPercent=[30,10,15,15,15,15]
 
-def get_weights(cus_no):
+def getWeights(cus_no):
 
     arr=[]
-    for i in range(len(listB)):
-        s="select "+listB[i]+", count(*) as cnt from post where account= '"+cus_no+"' Group by "+listB[i]+";"
+    for i in range(len(listPostStyleOption)):
+        SQLCmd="select "+listPostStyleOption[i]+", count(*) as cnt from post where account= '"+cus_no+"' Group by "+listPostStyleOption[i]+";"
 
-        style_sum=0
-        row_max=0
-        for j in cursor2List(s):
+        styleSum=0
+        rowMax=0
+        for j in cursorToList(SQLCmd):
             # 加總
-            style_sum+=int(j[1])
-            row_max+=1
+            styleSum+=int(j[1])
+            rowMax+=1
 
         # 算比例
-        print(style_sum/row_max)
-        arr.append(style_sum/row_max)
+        print(styleSum/rowMax)
+        arr.append(styleSum/rowMax)
     print(len(arr))
 
-get_weights("44")
-
-
-
-
-
-# ST02 = cursor.fetchall()
-# print(ST02)
+getWeights("44")
