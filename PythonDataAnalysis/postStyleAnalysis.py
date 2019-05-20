0f28f8ace138884c38b45541208f1937c96fede7
Eason Fang <10456009@ntub.edu.tw>
2019-05-20 21:41:05 +0800

    Update Python Data Analysis

--- PythonDataAnalysis/postStyleAnalysis.py
+++ PythonDataAnalysis/postStyleAnalysis.py
@@ -13,22 +13,31 @@ def cursorToList(SQLCmd):
         listResult.append(listCut)
     return listResult
 
+# iWearDB = pyodbc.connect(
+#     'DRIVER={SQL Server}; SERVER=140.131.114.241,1433; DATABASE=108-510; UID=108iwear; PWD=@108iwear',
+#     ENGINE = "sql_server.pyodbc",
+#     NAME= "108-510",
+#     HOST="140.131.114.241,1433",
+#     USER="108iwear",
+#     PASSWORD="@108iwear",
+# )
+
 iWearDB = pyodbc.connect(
-    'DRIVER={SQL Server}; SERVER=140.131.114.241,1433; DATABASE=108-510; UID=108iwear; PWD=@108iwear',
+    'DRIVER={SQL Server}; SERVER=220.135.161.251,1433; DATABASE=108-510; UID=sa; PWD=sqlserver',
     ENGINE = "sql_server.pyodbc",
     NAME= "108-510",
-    HOST="140.131.114.241,1433",
-    USER="108iwear",
-    PASSWORD="@108iwear",
+    HOST="220.135.161.251,1433",
+    USER="sa",
+    PASSWORD="sqlserver",
 )
 
 cursor = iWearDB.cursor()
 
-for row in cursorToList("SELECT memInform.account, memInform.gender, memInform.birth, memInform.height, memInform.weight, post.word, post.photo, post.styleNo, post.accessoriesNo, post.clothesNo, post.coatNo, post.pantsNo, post.shoesNo FROM dbo.memInform AS memInform LEFT JOIN dbo.post AS post ON memInform.account=post.account"):
-    MyJieba_hant.MyJieba_hant(context=row[5])
+# for row in cursorToList("SELECT memInform.account, memInform.gender, memInform.birth, memInform.height, memInform.weight, post.word, post.photo, post.styleNo, post.accessoriesNo, post.clothesNo, post.coatNo, post.pantsNo, post.shoesNo FROM dbo.memInform AS memInform LEFT JOIN dbo.post AS post ON memInform.account=post.account"):
+#     MyJieba_hant.MyJieba_hant(context=row[5])
 
 listPostStyleOption=['styleNo','accessoriesNo','clothesNo','coatNo','pantsNo','shoesNo']
-listPercent=[30,10,15,15,15,15]
+listPercent=[0.3,0.1,0.15,0.15,0.15,0.15]
 
 def getWeights(cusNo):
 
@@ -44,8 +53,7 @@ def getWeights(cusNo):
             rowMax+=1
 
         # 算比例
-        print(styleSum/rowMax)
+        print(styleSum/rowMax*listPercent[i])
         arr.append(styleSum/rowMax)
     print(len(arr))
-
-getWeights("44")
+getWeights("64")
