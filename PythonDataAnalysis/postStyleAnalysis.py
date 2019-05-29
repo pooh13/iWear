45680814e114fc9abddeaeb5effc1f9c07906b9c
Eason Fang <10456009@ntub.edu.tw>
2019-05-29 21:05:31 +0800

    Update Python Data Analysis

--- PythonDataAnalysis/postStyleAnalysis.py
+++ PythonDataAnalysis/postStyleAnalysis.py
@@ -1,10 +1,11 @@
+import os
 import SQLServerDBLink
 
 # SQLServerDBLinkMyDB
-# iWearDB = SQLServerDBLink.SQLServerDBLinkMyDB()
+iWearDB = SQLServerDBLink.SQLServerDBLinkMyDB()
 
 # SQLServerDBLink241DB
-iWearDB = SQLServerDBLink.SQLServerDBLink241DB()
+# iWearDB = SQLServerDBLink.SQLServerDBLink241DB()
 
 def cursorToList(cusNoPostCntSortSQLCmd):
     cursor.execute(cusNoPostCntSortSQLCmd)
@@ -62,6 +63,16 @@ def getPossible(cusNo):
     # 列印出(SELECT COUNT(*) as cnt,styleNo,accessoriesNo,clothesNo,coatNo,pantsNo,shoesNo FROM post WHERE account =64 GROUP BY styleNo,accessoriesNo,clothesNo,coatNo,pantsNo,shoesNo ORDER BY styleNo,accessoriesNo,clothesNo,coatNo,pantsNo,shoesNo)
     print(cusNoPostCntSortA)
 
+    # 判斷 MyDB 是否有原有輸出檔案存在
+    filePathMyDB = "DataAnalysisResult/analysisResultMyDB.txt"
+    if os.path.isfile(filePathMyDB):
+        os.remove(filePathMyDB)
+
+    # 判斷 241DB 是否有原有輸出檔案存在
+    # filePath241DB = "DataAnalysisResult/analysisResult241DB.txt"
+    # if os.path.isfile(filePath241DB):
+    #     os.remove(filePath241DB)
+
     # 抓取且執行 cusNoPostCntSortA 查詢結果 = arrPersonalRow
     for arrPersonalRow in cursorToList(cusNoPostCntSortA):
         cusNoPostCntSortB=""
@@ -76,12 +87,12 @@ def getPossible(cusNo):
         # 列印出每一種可能及比例
         print(cusNoPostCntSortB)
 
-        # 將分析結果寫成檔案 localhost DB
-        # with open('analysisResultlocalDB.txt',"a",encoding='UTF-8') as printFile:
-        #     printFile.write(cusNoPostCntSortB+"\n")
+        # 將分析結果寫成檔案 MyDB
+        with open('DataAnalysisResult/analysisResultMyDB.txt',"a",encoding='UTF-8') as printFile:
+            printFile.write(cusNoPostCntSortB+"\n")
 
-        # 將分析結果寫成檔案 241 DB
-        # with open('analysisResult241sDB.txt',"a",encoding='UTF-8') as printFile:
+        # 將分析結果寫成檔案 241DB
+        # with open('DataAnalysisResult/analysisResult241DB.txt',"a",encoding='UTF-8') as printFile:
         #     printFile.write(cusNoPostCntSortB+"\n")
 
 getPossible("64")
